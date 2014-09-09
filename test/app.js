64b71fa4d2a519a298f5c26764e6f4ea1b61bdb2
mauricepruna <mprun003@fiu.edu>
2014-09-09 13:01:38 -0400

    Documentation folderand test folder

---
+++ test/app.js
@@ -0,0 +1,105 @@
+/*
+    This file is generated and updated by Sencha Cmd. You can edit this file as
+    needed for your application, but these edits will have to be merged by
+    Sencha Cmd when it performs code generation tasks such as generating new
+    models, controllers or views and when running "sencha app upgrade".
+
+    Ideally changes to this file would be limited and most work would be done
+    in other places (such as Controllers). If Sencha Cmd cannot merge your
+    changes and its generated code, it will produce a "merge conflict" that you
+    will need to resolve manually.
+*/
+
+Ext.application({
+    name: 'PeopleMover',
+
+  
+
+    launch: function() {
+    	 Ext.create('Ext.data.TreeStore', {
+            storeId: 'TreeStore',
+            fields: ['title', 'link', 'author', 'contentSnippet', 'content', {
+                name: 'leaf',
+                defaultValue: true
+            }],
+            root: {
+                leaf: false
+            },
+            proxy: {
+                type: 'jsonp',
+                url: 'https://ajax.googleapis.com/ajax/services/feed/load?v=1.0&q=http://feeds.feedburner.com/SenchaBlog',
+                reader: {
+                    type: 'json',
+                    rootProperty: 'responseData.feed.entries'
+                }
+            }
+        });        
+	
+        Ext.create("Ext.tab.Panel", {
+            fullscreen: true,
+            tabBarPosition: 'bottom',
+
+            items: [
+               {
+                    xtype: 'nestedlist',
+                    title: 'Home',
+                    iconCls: 'home',
+                    displayField: 'title',
+
+                    store: 'TreeStore',
+
+                    detailCard: {
+                        xtype: 'panel',
+                        scrollable: true,
+                        styleHtmlContent: true
+                    },
+
+                    listeners: {
+                        itemtap: function(nestedList, list, index, element, post) {
+                            this.getDetailCard().setHtml(post.get('content'));
+                        }
+                    }
+                },
+                // this is the new item
+                {
+                    title: 'Contact',
+                    iconCls: 'user',
+                    xtype: 'formpanel',
+                    url: 'contact.php',
+                    layout: 'vbox',
+
+                    items: [
+                        {
+                            xtype: 'fieldset',
+                            title: 'Contact Us',
+                            instructions: '(email address is optional)',
+                            height: 285,
+                            items: [
+                                {
+                                    xtype: 'textfield',
+                                    label: 'Name'
+                                },
+                                {
+                                    xtype: 'emailfield',
+                                    label: 'Email'
+                                },
+                                {
+                                    xtype: 'textareafield',
+                                    label: 'Message'
+                                }
+                            ]
+                        },
+                        {
+                            xtype: 'button',
+                            text: 'Send',
+                            ui: 'confirm',
+                            handler: function() {
+                                this.up('formpanel').submit();
+                            }
+                        }
+                    ]
+                }
+            ]
+        });    
+    }
+});
