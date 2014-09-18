7cf56ffa12449214327d03a5ec18f204bf6d9195
mauricepruna <mprun003@fiu.edu>
2014-09-17 20:19:46 -0400

    removing touch II

---
+++ peoplemover/app/model/Item.js
@@ -0,0 +1,9 @@
+Ext.define('PeopleMover.model.Item', {
+     extend: 'Ext.data.Model',
+        config: {
+            fields: [{
+                name: 'text',
+                type: 'string'
+            }]
+        }
+});
