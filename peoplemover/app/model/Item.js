ccb15f167c4de5455f14e4f2a569c8484b528eaa
mauricepruna <mprun003@fiu.edu>
2014-10-21 19:46:49 -0400

    deleted peoplemover

--- peoplemover/app/model/Item.js
+++
@@ -1,9 +0,0 @@
-Ext.define('PeopleMover.model.Item', {
-     extend: 'Ext.data.Model',
-        config: {
-            fields: [{
-                name: 'text',
-                type: 'string'
-            }]
-        }
-});
